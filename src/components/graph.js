3f78057039a001d1e37b08fa6477470f07d8ff15
VishnuSastryHK <hkvsastry3300@gmail.com>
2022-07-18 13:45:17 +0530

    Styling the graphs

--- src/components/graph.js
+++ src/components/graph.js
@@ -37,6 +37,7 @@ function Graph() {
   let phList=[];
   let tempList=[];
   let timestampList=[];
+  let wqiList=[];
   // eslint-disable-next-line array-callback-return
   waterData.map((data) => {
     doList.push(data.do);
@@ -44,6 +45,7 @@ function Graph() {
     turbidityList.push(data.turbidity);
     phList.push(data.ph);
     tempList.push(data.temp);
+    wqiList.push(data.wqi);
     timestampList.push(data.timestamp);
   });
   
@@ -55,7 +57,7 @@ function Graph() {
   const data_do = {
     labels: timestampList,
     datasets: [{
-      label: 'Dissolved Oxygen(DO)',
+      label: 'Dissolved Oxygen(DO) [mg/l]',
       data: doList,
       fill: true,
       borderColor: 'rgba(255,99,132,1)',
@@ -65,7 +67,7 @@ function Graph() {
   const data_tds = {
     labels: timestampList,
     datasets: [{
-      label: 'TDS',
+      label: 'Total Dissolved Salts(TDS) [mg/l]',
       data: tdsList,
       fill: true,
       borderColor: 'rgba(255, 206, 86, 1)',
@@ -75,7 +77,7 @@ function Graph() {
   const data_turbidity = {
     labels: timestampList,
     datasets: [{
-      label: 'Turbidity',
+      label: 'Turbidity [NTU]',
       data: turbidityList,
       fill: true,
       borderColor: 'rgba(153, 102, 255, 1)',
@@ -95,13 +97,66 @@ function Graph() {
   const data_temp = {
     labels: timestampList,
     datasets: [{
-      label: 'Temperature',
+      label: 'Temperature [ºC]',
       data: tempList,
       fill: true,
       borderColor: 'rgba(255, 159, 64, 1)',
       backgroundColor: 'rgba(255, 159, 64, 0.2)',
     }]
   };
+  const data_wqi = {
+    labels: timestampList,
+    datasets: [{
+      label: 'Water Quality Index(WQI)',
+      data: wqiList,
+      fill: true,
+      borderColor: 'rgba(128,128,1,1)',
+      backgroundColor: 'rgba(128,128,0.2,0.2)',
+    }]
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      tooltip: {
+        mode: 'index',
+        intersect: false
+      },
+      // title: {
+      //   display: true,
+      //   text: 'Chart.js Line Chart'
+      // },
+      legend: {
+        labels: {
+          usePointStyle: true,
+          pointStyle: 'circle',
+          boxWidth: 10,
+          font:{size: 18, lineHeight: 12},
+        }
+      }
+    },
+    hover: {
+      mode: 'index',
+      intersect: false
+    },
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: 'TimeStamp'
+        }
+      },
+      y: {
+        title: {
+          display: true,
+          text: 'Value'
+        },
+        
+      }
+    }
+      
+  }
+
   const customStyles = {
     content : {
       top                   : '50%',
@@ -111,7 +166,7 @@ function Graph() {
       marginRight           : '-50%',
       transform             : 'translate(-50%, -50%)',
       backgroundColor       : '#F7CAC9',      
-      width:'50%',
+      //width:'50%',
       height: '50%',
       border: 'black',
     }
@@ -124,34 +179,34 @@ function Graph() {
       <center><h1>Statistics</h1></center>
       <div class="row">
         <div class="col"> 
-          <Line data={data_do}/>
+          <Line data={data_do} options={options}/>
         </div>
         <div class="col"> 
-          <Line data={data_tds}/>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col"> 
-          <Line data={data_turbidity}/>
-        </div>
-        <div class="col"> 
-          <Line data={data_ph}/>
+          <Line data={data_tds} options={options}/>
         </div>
       </div>
       <div class="row">
         <div class="col"> 
-          <Line data={data_temp}/>
+          <Line data={data_turbidity} options={options}/>
         </div>
         <div class="col"> 
-          {/* <Line data={data_ph}/> */}
+          <Line data={data_ph} options={options}/>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col"> 
+          <Line data={data_temp} options={options}/>
+        </div>
+        <div class="col"> 
+          <Line data={data_wqi}  options={options}/>
         </div>
       </div>
     </div>
     <center>
-    <button onClick={setModalIsOpenToTrue}>Click to Open Modal</button>
+    <button id="openModalBtn" onClick={setModalIsOpenToTrue}>Click to View Latest Data</button>
     </center>
 
-    <Modal isOpen={modalIsOpen} style={customStyles} onRequestClose={()=> setModalIsOpen(false)}>
+    <Modal isOpen={modalIsOpen} style={customStyles} id="modal" onRequestClose={()=> setModalIsOpen(false)}>
        <right><button className='cross' onClick={setModalIsOpenToFalse}>x</button></right>
        <div className='modal_data'>
        <center>
